d6c3b049f6ddc417452ed7f0b59a2f0fc4417bbd
George Riabov <george.riabov3105@gmail.com>
2025-11-25 18:23:03 +0100

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '53375249-7d769a3dfab07f88475703207';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  return axios.get(BASE_URL, { params }).then(response => response.data);
+}
